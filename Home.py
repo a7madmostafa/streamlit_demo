106138052e8497c97bd8ed01df68d786135a1f37
Ahmad Mostafa <a.m_eg@yahoo.com>
2022-09-24 20:13:35 +0200

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title("Hello World")
+
+st.write("This is a test")
+
+st.markdown(''' 
+                * First item
+                * Second item
+                * Third item
+                * Fourth item   ''')
+
+st.markdown(''' 
+                1. This is a test
+                2. This is a test
+                3. This is a test   ''')
